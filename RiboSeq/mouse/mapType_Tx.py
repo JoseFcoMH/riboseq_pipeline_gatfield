1ddfe3902a1cacef47baf680dd8a0cfd8aa8b430
Virginie Ricci <ninet93@hotmail.com>
2026-01-05 14:07:25 +0100

    Update mapType_Tx.py

--- RiboSeq/mouse/mapType_Tx.py
+++ RiboSeq/mouse/mapType_Tx.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+### LICENCE
+# Copyright 2024-2025 Virginie Ricci
+
+# This file is part of Snakemake_pipeline (https://github.com/gatfieldlab/Snakemake_pipeline/).
+
+# Snakemake_pipeline is free software: you can redistribute it and/or modify it under the terms of the 
+# GNU General Public License as published by the Free Software Foundation, 
+# either version 3 of the License, or (at your option) any later version.
+
+# Snakemake_pipeline is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with Snakemake_pipeline. 
+# If not, see <https://www.gnu.org/licenses/>.
+### LICENCE 
+
 from __future__ import print_function
 import sys
 import re
