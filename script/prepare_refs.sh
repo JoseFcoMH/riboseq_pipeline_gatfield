d5f011b3ba64cc4359eb8caba98a26d95fd65dda
Virginie Ricci <ninet93@hotmail.com>
2026-01-05 14:05:43 +0100

    Update prepare_refs.sh

--- script/prepare_refs.sh
+++ script/prepare_refs.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+### LICENCE
+# Copyright 2024-2025 Virginie Ricci
+
+# This file is part of Snakemake_pipeline (https://github.com/gatfieldlab/Snakemake_pipeline/).
+
+# Snakemake_pipeline is free software: you can redistribute it and/or modify it under the terms of the 
+# GNU General Public License as published by the Free Software Foundation, 
+# either version 3 of the License, or (at your option) any later version.
+
+# Snakemake_pipeline is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with Snakemake_pipeline. 
+# If not, see <https://www.gnu.org/licenses/>.
+### LICENCE 
+
 # module load samtools/1.9
 # module load STAR/2.7.11b
 # module load python3/3.7.3
