9addb476a7b1eb59c18a86835c1a74d2cbe386ef
Virginie Ricci <ninet93@hotmail.com>
2026-01-14 13:32:08 +0100

    Update prepare_refs.sh

--- script/prepare_refs.sh
+++ script/prepare_refs.sh
@@ -44,7 +44,6 @@ path_ref=/data/databases/mouse/
 #tRNA_link=https://gtrnadb.ucsc.edu/genomes/eukaryota/Mmusc39/mm39-tRNAs.fa # previously this link
 tRNA_link=https://gtrnadb.org/genomes/eukaryota/Mmusc39/mm39-tRNAs.fa 
 
-
 preribo_45S='NR_046233.2'
 ### TO EDIT ### TO EDIT ### TO EDIT ### TO EDIT ### TO EDIT ### TO EDIT 
 ### TO EDIT ### TO EDIT ### TO EDIT ### TO EDIT ### TO EDIT ### TO EDIT 
