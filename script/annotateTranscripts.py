3f2576386e03b3c8614483f875e92e43102e58a9
Virginie Ricci <ninet93@hotmail.com>
2026-01-23 16:54:42 +0100

    Update annotateTranscripts.py

--- script/annotateTranscripts.py
+++ script/annotateTranscripts.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+### LICENCE
+# Copyright (c) 2024-2025 Virginie Ricci
+
+# This file is part of Snakemake_pipeline (https://github.com/gatfieldlab/Snakemake_pipeline/).
+
+# Snakemake_pipeline is free software: you can redistribute it and/or modify it under the terms of the 
+# GNU General Public License as published by the Free Software Foundation, 
+# either version 3 of the License, or (at your option) any later version.
+
+# Snakemake_pipeline is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with Snakemake_pipeline. 
+# If not, see <https://www.gnu.org/licenses/>.
+### LICENCE 
+
+
 ## This script is used to caclulate the translation start and stop
 ## positions in a transcript given a GFF file.
 
