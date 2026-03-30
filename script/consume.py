ac3edaaf0681e4fc5b229d86df60f6abe1a92914
Virginie Ricci <ninet93@hotmail.com>
2026-03-30 14:49:27 +0200

    Update consume.py

--- script/consume.py
+++ script/consume.py
@@ -18,7 +18,7 @@
 ### LICENCE 
 
 """
-A simple size filter for fastq formatted outputs
+A simple size filter for fastq formatted outputs based read size (MIN, MAX)
 """
 
 import sys
@@ -35,41 +35,38 @@ __status__ = "Development"
 
 VERSION = __version__
 
-def main():
-    """
-    Filters incoming fastq input based read size (MIN, MAX)
-    """
-    if len(sys.argv) > 1 and sys.argv[1] == '-v':
-        sys.stdout.write("consume {}\n".format(VERSION))
-        sys.exit(0)
-    try:
-        min_seq_len = int(sys.argv[1])
-    except IndexError:
-        min_seq_len = 21 #for RB, 26
-    try:
-        max_seq_len = int(sys.argv[2])
-    except IndexError:
-        max_seq_len = 60 #for RB, 35
 
-    total_count = 0
-    filtered_count = 0
+if len(sys.argv) > 1 and sys.argv[1] == '-v':
+    sys.stdout.write("consume {}\n".format(VERSION))
+    sys.exit(0)
+try:
+    min_seq_len = int(sys.argv[1])
+except IndexError:
+    min_seq_len = 21 #for RB, 26
+try:
+    max_seq_len = int(sys.argv[2])
+except IndexError:
+    max_seq_len = 60 #for RB, 35
 
-    # to account for newline character
-    eff_min_seq_len = min_seq_len + 1
-    eff_max_seq_len = max_seq_len + 1
-    if len(sys.argv) > 3:
-        outfile = open(sys.argv[3], 'w')
+total_count = 0
+filtered_count = 0
+
+# to account for newline character
+eff_min_seq_len = min_seq_len + 1
+eff_max_seq_len = max_seq_len + 1
+if len(sys.argv) > 3:
+    outfile = open(sys.argv[3], 'w')
+else:
+    outfile = sys.stdout
+
+for identifier, seq, sep, qual in itertools.zip_longest(*[sys.stdin]*4):
+    total_count += 1
+    if len(seq) < eff_min_seq_len or len(seq) > eff_max_seq_len:
+        filtered_count += 1
     else:
-        outfile = sys.stdout
-
-    for identifier, seq, sep, qual in itertools.zip_longest(*[sys.stdin]*4):
-        total_count += 1
-        if len(seq) < eff_min_seq_len or len(seq) > eff_max_seq_len:
-            filtered_count += 1
-        else:
-            outfile.write(identifier+seq+sep+qual)
-    outfile.close()
-    sys.stderr.write("size_filter.py version {}\n".format(VERSION))
-    sys.stderr.write("Total number of sequences: {}\n".format(total_count))
-    sys.stderr.write("Number of sequences filtered out: {}\n".format(filtered_count))
-    sys.stderr.write("Filter used: [min:{}, max:{}]\n".format(min_seq_len, max_seq_len))
+        outfile.write(identifier+seq+sep+qual)
+outfile.close()
+sys.stderr.write("size_filter.py version {}\n".format(VERSION))
+sys.stderr.write("Total number of sequences: {}\n".format(total_count))
+sys.stderr.write("Number of sequences filtered out: {}\n".format(filtered_count))
+sys.stderr.write("Filter used: [min:{}, max:{}]\n".format(min_seq_len, max_seq_len))
